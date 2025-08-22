29726bf4614b8ce6267c2c9a55bca1b4cef6004a
dadasese <157042557+dadasese@users.noreply.github.com>
2025-08-21 20:54:16 -0500

    implement patient update endpoint

--- patient-service/src/main/java/com/shibuya/backend/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/shibuya/backend/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.shibuya.backend.patientservice.dto;
 
+import com.shibuya.backend.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,7 +21,8 @@ public class PatientRequestDTO {
     @NotNull(message = "Date of birth is required")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class,
+    message = "Registered date is required")
     private String registeredDate;
 
     public String getName() {
--- patient-service/src/main/java/com/shibuya/backend/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/shibuya/backend/patientservice/service/PatientService.java
@@ -3,12 +3,15 @@ package com.shibuya.backend.patientservice.service;
 import com.shibuya.backend.patientservice.dto.PatientRequestDTO;
 import com.shibuya.backend.patientservice.dto.PatientResponseDTO;
 import com.shibuya.backend.patientservice.exception.EmailExceptionHandler;
+import com.shibuya.backend.patientservice.exception.PatientNotFoundException;
 import com.shibuya.backend.patientservice.mapper.PatientMapper;
 import com.shibuya.backend.patientservice.model.Patient;
 import com.shibuya.backend.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -31,7 +34,30 @@ public class PatientService {
         }
         Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
-
         return PatientMapper.toDTO(patient);
     }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+
+        Patient patient = patientRepository.findById(id).orElseThrow(
+                () -> new PatientNotFoundException("Patient not found with ID: " + id));
+
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)) {
+            throw new EmailExceptionHandler("Email " + patientRequestDTO.getEmail() + " already exists");
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatePatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(updatePatient);
+    }
+
+    private void extracted(PatientRequestDTO patientRequestDTO) {
+
+    }
+
+
 }
